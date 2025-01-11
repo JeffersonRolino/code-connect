d578947c4e2bbeff9dab1a4f195cc9ad946dca15
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-11 11:55:55 -0300

    Capítulo 02 - Aula 04: Funções assíncronas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const uploadBtn = document.getElementById("upload-btn");
-const inputUpload = document.getElementById("imagem-upload");
+const inputUpload = document.getElementById("image-upload");
 
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
@@ -19,3 +19,20 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo);
     });
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo");
+        }
+    }
+});
